fe5577cb9f673ff9f4a30b3b1f47438b3b1e81c2
Mark Zalass <markzalass@gmail.com>
2020-03-26 14:34:48 -0400

    Added txt files, added process class, added reading processes from file

--- Assignment 3/src/coen346assignment3/main/Driver.java
+++ Assignment 3/src/coen346assignment3/main/Driver.java
@@ -1,10 +1,58 @@
 package coen346assignment3.main;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+
+		// Processes read from file, line from input file stored in array list
+		ArrayList<String> inProcesses = readInput("processes.txt");
+		System.out.println(inProcesses); // Debug statement to check if file read properly
+
+		// Number of processes defined
+		int processNum = inProcesses.size();
+		Process[] processArray = createProcesses(processNum, inProcesses);
+
+		// Output file created and System.out set to output to file
+		/*PrintStream output = new PrintStream(new File("output.txt"));
+		System.setOut(output);*/
 
 	}
 
+
+	/* Method to read input file line by line */
+	public static ArrayList<String> readInput(String fileName) {
+		ArrayList<String> inLine = new ArrayList<>();
+		try {
+			InputStream is = new FileInputStream(fileName); // Input stream from file opened
+			BufferedReader br = new BufferedReader(new InputStreamReader(is)); // Line stored in buffered reader
+			String readLine; // String storing the line read
+			while ((readLine = br.readLine()) != null) {
+				inLine.add(readLine); // Line read added to array list of all processes (lines read)
+			}
+			br.close();
+		} catch (Exception e) { // If file fails to be read, exception generated and error message shown
+			e.printStackTrace();
+			System.err.println("Error reading file");
+		}
+		return inLine;
+	}
+
+	/* Method to create process objects from info from file and return array of processes */
+	public static Process[] createProcesses(int processNum, ArrayList<String> newProcesses) {
+		// Array of processes created
+		Process[] processes = new Process[processNum];
+
+		// Arrival time and burst time separated and used to create process objects
+		for (int i = 0; i < processNum; i++) {
+			String[] split = newProcesses.get(i).split(" ");
+			int arrivalTime = Integer.parseInt(split[0]);
+			int burstTime = Integer.parseInt(split[1]);
+			Process p = new Process(arrivalTime, burstTime, i); // New process created
+			processes[i] = p; // New process added to processes array
+		}
+		return processes;
+	}
 }
---
+++ Assignment 3/src/coen346assignment3/main/Process.java
@@ -0,0 +1,73 @@
+package coen346assignment3.main;
+
+public class Process implements Runnable{
+    /* Variables for process*/
+    private int arrivalTime;
+    private float remainingTime;
+    private float time;
+    private volatile Boolean hasCPU = false;
+    private Boolean isFinished = false;
+    private float quantum = 3000; // Can be constant for this assignment
+    private float runTime = 0;
+    private float timeToRun;
+    private Boolean hasRun = false;
+    private int processID;
+
+    /* Constructor */
+    Process(int arrivalTime, int burstTime, int processID) {
+        this.arrivalTime = arrivalTime;
+        this.remainingTime = burstTime;
+        this.processID = processID;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            while (!hasCPU || isFinished) Thread.onSpinWait(); // If thread starts but process does not have CPU, wait until given CPU
+            timeToRun = Math.min(remainingTime, quantum); // Process will run for either the whole quantum or earlier if time remaining is less than the quantum
+            time += timeToRun; // Time increments
+            remainingTime -= timeToRun; // Time ran deducted from time remaining for the process to execute
+            runTime += timeToRun; // Time to run (time process ran in quantum) added to total time ran
+
+            hasCPU = false; // Process relinquishes CPU back to the scheduler
+
+            if (remainingTime <= 0) { // If the process is done
+                isFinished = true; // Process signals to scheduler that it is done execution
+            }
+        }
+    }
+
+    /* Getters */
+    public Boolean getFinished() {
+        return isFinished;
+    }
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+    public float getTimeToRun() {
+        return timeToRun;
+    }
+    public Boolean getHasCPU() {
+        return hasCPU;
+    }
+    public Boolean getHasRun() {
+        return hasRun;
+    }
+    public int getProcessID() {
+        return processID;
+    }
+    public float getRemainingTime() {
+        return remainingTime;
+    }
+
+    /* Setters */
+    public void setHasCPU(Boolean hasCPU) {
+        this.hasCPU = hasCPU;
+    }
+    public void setTime(float time) {
+        this.time = time;
+    }
+    public void setHasRun(Boolean hasRun) {
+        this.hasRun = hasRun;
+    }
+}
